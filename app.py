d2e402e65e153891de379b42ccba5cce6eeeae63
M15160058 <132909759+M15160058@users.noreply.github.com>
2026-02-13 18:12:55 -0500

    Update app.py

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ components.html(
 <script>
 const targetLat = 39.132473;
 const targetLng = -84.5170492;
-const allowedRadius = 3000;
+const allowedRadius = 60;
 
 const formURL = "https://forms.office.com/Pages/DesignPageV2.aspx?origin=NeoPortalPage&subpage=design&id=bC4i9cZf60iPA3PbGCA7Y3zURXDN2c1Mk8io1jX0SGNUMlVIUEtTQ0xaWEUxTDFZMjUzM0xLUFVJVC4u";
 
